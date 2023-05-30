addbf93fe661d436b7f926265266ef846624d1d5
Duyen29 <k205480106033@tnut.edu.vn>
2023-05-30 13:21:22 +0700

    bài tập chưa làm xong

---
+++ LIENKET_CSDL/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace LIENKET_CSDL
+{
+    partial class hoadon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtLog = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.text_MAKH = new System.Windows.Forms.TextBox();
+            this.text_NGAYTHANG = new System.Windows.Forms.TextBox();
+            this.text_MAHANG = new System.Windows.Forms.TextBox();
+            this.text_SOHD = new System.Windows.Forms.TextBox();
+            this.txt_MAKH = new System.Windows.Forms.Label();
+            this.txt_NGAYTHANG = new System.Windows.Forms.Label();
+            this.txt_MAHANG = new System.Windows.Forms.Label();
+            this.txt_SOHD = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmd_them = new System.Windows.Forms.Button();
+            this.cmd_sua = new System.Windows.Forms.Button();
+            this.cmd_xoa = new System.Windows.Forms.Button();
+            this.cmd_view = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(61, 4);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.groupBox1.Controls.Add(this.text_MAKH);
+            this.groupBox1.Controls.Add(this.text_NGAYTHANG);
+            this.groupBox1.Controls.Add(this.text_MAHANG);
+            this.groupBox1.Controls.Add(this.text_SOHD);
+            this.groupBox1.Controls.Add(this.txt_MAKH);
+            this.groupBox1.Controls.Add(this.txt_NGAYTHANG);
+            this.groupBox1.Controls.Add(this.txt_MAHANG);
+            this.groupBox1.Controls.Add(this.txt_SOHD);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.groupBox1.Location = new System.Drawing.Point(43, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(735, 143);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // text_MAKH
+            // 
+            this.text_MAKH.BackColor = System.Drawing.Color.Pink;
+            this.text_MAKH.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_MAKH.Location = new System.Drawing.Point(556, 95);
+            this.text_MAKH.Multiline = true;
+            this.text_MAKH.Name = "text_MAKH";
+            this.text_MAKH.Size = new System.Drawing.Size(113, 41);
+            this.text_MAKH.TabIndex = 7;
+            // 
+            // text_NGAYTHANG
+            // 
+            this.text_NGAYTHANG.BackColor = System.Drawing.Color.Pink;
+            this.text_NGAYTHANG.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_NGAYTHANG.Location = new System.Drawing.Point(553, 19);
+            this.text_NGAYTHANG.Multiline = true;
+            this.text_NGAYTHANG.Name = "text_NGAYTHANG";
+            this.text_NGAYTHANG.Size = new System.Drawing.Size(116, 37);
+            this.text_NGAYTHANG.TabIndex = 6;
+            // 
+            // text_MAHANG
+            // 
+            this.text_MAHANG.BackColor = System.Drawing.Color.Pink;
+            this.text_MAHANG.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_MAHANG.Location = new System.Drawing.Point(132, 96);
+            this.text_MAHANG.Multiline = true;
+            this.text_MAHANG.Name = "text_MAHANG";
+            this.text_MAHANG.Size = new System.Drawing.Size(119, 41);
+            this.text_MAHANG.TabIndex = 5;
+            // 
+            // text_SOHD
+            // 
+            this.text_SOHD.BackColor = System.Drawing.Color.Pink;
+            this.text_SOHD.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_SOHD.Location = new System.Drawing.Point(132, 19);
+            this.text_SOHD.Multiline = true;
+            this.text_SOHD.Name = "text_SOHD";
+            this.text_SOHD.Size = new System.Drawing.Size(119, 41);
+            this.text_SOHD.TabIndex = 4;
+            // 
+            // txt_MAKH
+            // 
+            this.txt_MAKH.AutoSize = true;
+            this.txt_MAKH.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_MAKH.Location = new System.Drawing.Point(398, 97);
+            this.txt_MAKH.Name = "txt_MAKH";
+            this.txt_MAKH.Size = new System.Drawing.Size(158, 19);
+            this.txt_MAKH.TabIndex = 3;
+            this.txt_MAKH.Text = "MÃ KHÁCH HÀNG :";
+            // 
+            // txt_NGAYTHANG
+            // 
+            this.txt_NGAYTHANG.AutoSize = true;
+            this.txt_NGAYTHANG.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_NGAYTHANG.Location = new System.Drawing.Point(420, 25);
+            this.txt_NGAYTHANG.Name = "txt_NGAYTHANG";
+            this.txt_NGAYTHANG.Size = new System.Drawing.Size(123, 19);
+            this.txt_NGAYTHANG.TabIndex = 2;
+            this.txt_NGAYTHANG.Text = "NGÀY THÁNG :";
+            // 
+            // txt_MAHANG
+            // 
+            this.txt_MAHANG.AutoSize = true;
+            this.txt_MAHANG.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_MAHANG.Location = new System.Drawing.Point(36, 96);
+            this.txt_MAHANG.Name = "txt_MAHANG";
+            this.txt_MAHANG.Size = new System.Drawing.Size(96, 19);
+            this.txt_MAHANG.TabIndex = 1;
+            this.txt_MAHANG.Text = "MÃ HÀNG :";
+            // 
+            // txt_SOHD
+            // 
+            this.txt_SOHD.AutoSize = true;
+            this.txt_SOHD.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_SOHD.Location = new System.Drawing.Point(15, 25);
+            this.txt_SOHD.Name = "txt_SOHD";
+            this.txt_SOHD.Size = new System.Drawing.Size(118, 19);
+            this.txt_SOHD.TabIndex = 0;
+            this.txt_SOHD.Text = "SỐ HOÁ ĐƠN :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(306, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "THÔNG TIN HOÁ ĐƠN";
+            // 
+            // cmd_them
+            // 
+            this.cmd_them.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.cmd_them.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmd_them.Location = new System.Drawing.Point(82, 216);
+            this.cmd_them.Name = "cmd_them";
+            this.cmd_them.Size = new System.Drawing.Size(98, 33);
+            this.cmd_them.TabIndex = 0;
+            this.cmd_them.Text = "THÊM";
+            this.cmd_them.UseVisualStyleBackColor = false;
+            // 
+            // cmd_sua
+            // 
+            this.cmd_sua.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.cmd_sua.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmd_sua.Location = new System.Drawing.Point(247, 216);
+            this.cmd_sua.Name = "cmd_sua";
+            this.cmd_sua.Size = new System.Drawing.Size(93, 33);
+            this.cmd_sua.TabIndex = 4;
+            this.cmd_sua.Text = "SỬA";
+            this.cmd_sua.UseVisualStyleBackColor = false;
+            // 
+            // cmd_xoa
+            // 
+            this.cmd_xoa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.cmd_xoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmd_xoa.Location = new System.Drawing.Point(419, 216);
+            this.cmd_xoa.Name = "cmd_xoa";
+            this.cmd_xoa.Size = new System.Drawing.Size(97, 33);
+            this.cmd_xoa.TabIndex = 5;
+            this.cmd_xoa.Text = "XOÁ";
+            this.cmd_xoa.UseVisualStyleBackColor = false;
+            // 
+            // cmd_view
+            // 
+            this.cmd_view.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.cmd_view.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmd_view.Location = new System.Drawing.Point(589, 216);
+            this.cmd_view.Name = "cmd_view";
+            this.cmd_view.Size = new System.Drawing.Size(96, 33);
+            this.cmd_view.TabIndex = 6;
+            this.cmd_view.Text = "VIEW";
+            this.cmd_view.UseVisualStyleBackColor = false;
+            this.cmd_view.Click += new System.EventHandler(this.cmd_view_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(233, 282);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(251, 19);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "THÔNG TIN CHI TIẾT HOÁ ĐƠN";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(43, 321);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(630, 150);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LIENKET_CSDL.Properties.Resources.hinh_nen_may_tinh_de_thuong_1_800x4511;
+            this.pictureBox1.Location = new System.Drawing.Point(716, 321);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(182, 147);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // hoadon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(909, 480);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmd_view);
+            this.Controls.Add(this.cmd_xoa);
+            this.Controls.Add(this.cmd_sua);
+            this.Controls.Add(this.cmd_them);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "hoadon";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.hoadon_Load_1);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip txtLog;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox text_MAKH;
+        private System.Windows.Forms.TextBox text_NGAYTHANG;
+        private System.Windows.Forms.TextBox text_MAHANG;
+        private System.Windows.Forms.TextBox text_SOHD;
+        private System.Windows.Forms.Label txt_MAKH;
+        private System.Windows.Forms.Label txt_NGAYTHANG;
+        private System.Windows.Forms.Label txt_MAHANG;
+        private System.Windows.Forms.Label txt_SOHD;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button cmd_them;
+        private System.Windows.Forms.Button cmd_sua;
+        private System.Windows.Forms.Button cmd_xoa;
+        private System.Windows.Forms.Button cmd_view;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ LIENKET_CSDL/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+
+namespace LIENKET_CSDL
+{
+    public partial class hoadon : Form
+    {
+        public hoadon()
+        {
+            InitializeComponent();
+        }
+
+        String chuoiketnoi = @"Data Source=Admin\SQLEXPRESS;Initial Catalog=baitaplon;Integrated Security=True";
+        String sql;
+        SqlConnection ketnoi;
+        SqlCommand thuchien;
+        SqlDataReader docdulieu;
+        DataTable dt;
+        
+
+        String sohd;
+        private void hoadon_Load_1(object sender, EventArgs e)
+        {
+            ketnoi = new SqlConnection(chuoiketnoi);
+            hienthi();
+        }
+
+        public void hienthi()
+        {
+
+            ketnoi.Open();
+            sql = @"select SOHD,MAHANG,NGAYTHANG,MAKH from HD_BANHANG";
+            thuchien = new SqlCommand(sql, ketnoi);
+            docdulieu = thuchien.ExecuteReader();
+
+            dataGridView1 = new DataGridView();
+            dataGridView1.DataSource = docdulieu;
+            dataGridView1.DataSource = dataGridView1;
+            docdulieu.Close();
+            ketnoi.Close();
+        }
+
+        private void loatHD_BANHANG()
+        {
+            ketnoi = new SqlConnection(chuoiketnoi);
+
+            ketnoi.Open();
+            sql = "select* from HD_BANHANG ";
+            hienthi();
+            dataGridView1.DataSource = dt;
+            ketnoi.Close();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex == -1) { return; }
+            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+            sohd = row.Cells[0].Value.ToString();
+        }
+
+        private void cmd_view_Click(object sender, EventArgs e)
+        {
+            hienthi();
+            dataGridView1.DataSource = dt;
+           docdulieu.Close();
+            ketnoi.Close();
+
+        }
+
+        
+    }
+  
+}
